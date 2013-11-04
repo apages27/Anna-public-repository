25940ae52eb3c83c30a31d06a31f7d15a26e9d38
apages27 <apages27@gmail.com>
2013-11-04 18:10:47 -0500

    This program was a pop quiz from my apprenticeship.  The instructions were to make a dice-rolling program which starts with a "bankroll" of $100, where each roll of a pair of six-sided dice costs $1, and each roll of 7 earns $4, and print out the number of rolls until the bankroll reaches 0.

---
+++ Lucky7sAssignment/Lucky7sAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky7sAssignment
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            RollDice rd = new RollDice();
+            bool isOver = false;
+
+            while (!isOver)
+            {
+                Console.WriteLine("\n{0} dice rolls until broke.", rd.RolltheDice());
+                Console.Write("\nPlay again? Y/N ");
+
+                if (Console.ReadLine().ToUpper() == "N")
+                {
+                    isOver = true;
+                }
+            }
+        }
+    }
+}
---
+++ Lucky7sAssignment/Lucky7sAssignment/RollDice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky7sAssignment
+{
+    class RollDice
+    {
+        public int RolltheDice()
+        {
+            int rolls = 0;
+            int bankRoll = 100;
+            Random r = new Random();
+
+            while (bankRoll > 0)
+            {
+                int rollDie1 = r.Next(1, 7);
+                int rollDie2 = r.Next(1, 7);
+                rolls++;
+
+                if (rollDie1 + rollDie2 == 7)
+                {
+                    bankRoll = bankRoll + 3;
+                }
+                else
+                {
+                    bankRoll = bankRoll - 1;
+                }
+            }
+
+            return rolls;
+        }
+    }
+}
