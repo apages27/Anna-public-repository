b9aea68982386555a6a1a2cd4b7c44854fbb79c3
apages27 <apages27@gmail.com>
2013-11-04 18:21:34 -0500

    This was a pop quiz from my apprenticeship where the instructions were to create a console application where the computer picks a random number between 1 and 100, then has the user guess the number, and tells them after each guess whether they need to guess higher or lower.

---
+++ PopQuiz5GuessingGame/PopQuiz5GuessingGame/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+
+namespace PopQuiz5GuessingGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isGameOver = false;
+            int playerGuesses = 0;
+
+            while (!isGameOver)
+            {
+                int upperBoundPreset = int.Parse(ConfigurationManager.AppSettings["upperBound"]);
+                Random r = new Random();
+                int computerPick = r.Next(1, upperBoundPreset);
+                int userGuess = 0;
+                bool isCorrectGuess = false;
+                Console.Clear();
+
+                while (!isCorrectGuess)
+                {
+                    bool isNumber = false;
+
+                    while (!isNumber)
+                    {
+                        Console.Write("\nPlease enter a guess between 1-{0}: ", upperBoundPreset);
+
+                        isNumber = int.TryParse(Console.ReadLine(), out userGuess);
+                        if (!isNumber)
+                        {
+                            Console.WriteLine("\nThat's not a number!  Try again.");
+                        }
+                    }
+
+
+                    if (userGuess > computerPick)
+                    {
+                        Console.WriteLine("\nLower!");
+                        playerGuesses++;
+                    }
+                    else if (userGuess < computerPick)
+                    {
+                        Console.WriteLine("\nHigher!");
+                        playerGuesses++;
+                    }
+                    else
+                    {
+                        playerGuesses++;
+                        Console.WriteLine("\nYou got it!");
+                        Console.WriteLine("Number of guesses it took: {0}", playerGuesses);
+                        isCorrectGuess = true;
+                    }
+                }
+                Console.Write("\nPlay again? Y/N ");
+                if (Console.ReadLine().ToUpper() == "N")
+                {
+                    isGameOver = true;
+                }
+            }
+
+
+        }
+    }
+}
