4e6aa5d227f567a23569ea0b7eaec50e2d9521de
apages27 <apages27@gmail.com>
2016-04-24 12:47:17 -0400

    This is a little application I created for simulating third-party hardware I was developing against

---
+++ CineViewEmulatorForTCPTesting/CineViewEmulatorForTCPTesting/Emulator.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CineViewEmulatorForTCPTesting
+{
+    partial class Emulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.sendTestMsgButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(12, 91);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.Size = new System.Drawing.Size(498, 450);
+            this.resultsTextBox.TabIndex = 3;
+            // 
+            // sendTestMsgButton
+            // 
+            this.sendTestMsgButton.Location = new System.Drawing.Point(190, 37);
+            this.sendTestMsgButton.Name = "sendTestMsgButton";
+            this.sendTestMsgButton.Size = new System.Drawing.Size(126, 23);
+            this.sendTestMsgButton.TabIndex = 4;
+            this.sendTestMsgButton.Text = "Send Test Message";
+            this.sendTestMsgButton.UseVisualStyleBackColor = true;
+            this.sendTestMsgButton.Click += new System.EventHandler(this.sendTestMsgButton_Click);
+            // 
+            // Emulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 553);
+            this.Controls.Add(this.sendTestMsgButton);
+            this.Controls.Add(this.resultsTextBox);
+            this.Name = "Emulator";
+            this.Text = "TCP Emulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox resultsTextBox;
+        private System.Windows.Forms.Button sendTestMsgButton;
+    }
+}
+
---
+++ CineViewEmulatorForTCPTesting/CineViewEmulatorForTCPTesting/Emulator.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace CineViewEmulatorForTCPTesting
+{
+    public partial class Emulator : Form
+    {
+        private const int PortNumber = 9761;
+        private const string ServerIp = "127.0.0.1";
+        private readonly Dispatcher _dispatcher = Dispatcher.CurrentDispatcher;
+        private TcpClient _client;
+        private Dictionary<char, string> _dictionary;
+        private TcpListener _listener;
+
+        public Emulator()
+        {
+            InitializeComponent();
+
+            CreateASCIIHexDictionary();
+
+            CreateTCPServer();
+
+            Task.Run(() => { ListenForIncomingMessages(); });
+        }
+
+        private void CreateASCIIHexDictionary()
+        {
+            _dictionary = new Dictionary<char, string>
+            {
+                {'î', "0xee"},
+                {'g', "0x67"},
+                {'h', "0x68"},
+                {'j', "0x6A"},
+                {'k', "0x6B"},
+                {'Y', "0x59"},
+                {'Z', "0x5A"}
+            };
+        }
+
+        private void AppendMessage(string message, params object[] args)
+        {
+            var finalMessage = string.Format(message, args);
+
+            Action process =
+                () => { resultsTextBox.Text += $"{finalMessage}{Environment.NewLine}"; };
+
+            _dispatcher.BeginInvoke(process);
+        }
+
+        public void CreateTCPServer()
+        {
+            _listener = new TcpListener(IPAddress.Any, PortNumber);
+        }
+
+        private void ListenForIncomingMessages()
+        {
+            AppendMessage("Listening...");
+
+            while (true)
+            {
+                _listener.Start();
+
+                if (!_listener.Pending()) continue;
+
+                var streamReceived = ReceiveIncomingMessage();
+
+                var dataReceived = ParseIncomingMessage(streamReceived);
+
+                AppendMessage(string.Format("Received: " + dataReceived));
+
+                ResetListener();
+            }
+        }
+
+        private NetworkStream ReceiveIncomingMessage()
+        {
+            _client = _listener.AcceptTcpClient();
+
+            return _client.GetStream();
+        }
+
+        private void ResetListener()
+        {
+            _client?.Close();
+
+            _listener?.Stop();
+        }
+
+        private string ParseIncomingMessage(NetworkStream streamReceived)
+        {
+            var buffer = new byte[_client.ReceiveBufferSize];
+
+            var bytesRead = streamReceived.Read(buffer, 0, _client.ReceiveBufferSize);
+
+            var dataReceived =
+                ConvertASCIIToStringRepresentationOfHex(Encoding.Default.GetString(buffer, 0, bytesRead));
+
+            return dataReceived;
+        }
+
+        private string ConvertASCIIToStringRepresentationOfHex(string dataReceived)
+        {
+            var hexString = "";
+
+            foreach (var item in dataReceived)
+            {
+                if (_dictionary.ContainsKey(item))
+                    hexString += $"{_dictionary[item]} ";
+                else
+                    hexString += $"{item} ";
+            }
+
+            return hexString;
+        }
+
+        private void sendTestMsgButton_Click(object sender, EventArgs e)
+        {
+            var textToSend = DateTime.Now.ToString();
+
+            var client = new TcpClient(ServerIp, PortNumber);
+
+            var stream = client.GetStream();
+
+            var bytesToSend = Encoding.ASCII.GetBytes(textToSend);
+
+            AppendMessage(string.Format("Sending: " + textToSend));
+
+            stream.Write(bytesToSend, 0, bytesToSend.Length);
+        }
+    }
+}
