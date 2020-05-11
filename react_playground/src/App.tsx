e3d58a027db9dc6e2ca72ef5150d522ab3bbbb71
A.Pages <apages27@gmail.com>
2020-05-11 16:40:04 -0400

    Trying to hook up an API call

--- react_playground/src/App.tsx
+++ react_playground/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Clock from "./Clock-ico.svg";
 import './App.css';
 
@@ -9,7 +9,7 @@ class App extends React.Component {
 				<header className="App-header">
 					<img src={Clock} className="App-logo" alt="logo"/>
 					<Welcome name="Anna"/>
-					<DateFact month="12" day="1"/>
+					<DateFact/>
 				</header>
 			</div>
 		);
@@ -20,26 +20,37 @@ function Welcome(props: any) {
 	return <h1>Hello, {props.name}!</h1>;
 }
 
-function DateFact(props: any) {
-	let url: string = `https://numbersapi.p.rapidapi.com/${props.month}/${props.day}/date?fragment=false&json=false`;
-	let fact: any = "Initial value";
+function DateFact() {
+	useEffect(() => {
+		const fetchDateFact = async () => {
+			let url: string = `https://numbersapi.p.rapidapi.com/12/1/date?fragment=false&json=false`;
 
-	fetch(url, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "numbersapi.p.rapidapi.com",
-			"x-rapidapi-key": "0173102eccmsha12a431a4720adap1c04edjsn7c69820214b9"
-		}
-	})
-		.then(response => {
-			fact = response;
-		})
-		.catch(err => {
-			console.log(err);
-			fact = err;
-		});
+			try {
+				setData({fact: "Fetching a new date fact...", isFetching: true});
+				const response = await fetch(url, {
+					"method": "GET",
+					"headers": {
+						"x-rapidapi-host": "numbersapi.p.rapidapi.com",
+						"x-rapidapi-key": "0173102eccmsha12a431a4720adap1c04edjsn7c69820214b9"
+					}
+				});
+				console.log(`Response: `, response);
+				setData({fact: response.text, isFetching: false});
+			} catch (e) {
+				console.log(e);
+				setData({fact: e.text, isFetching: false});
+			}
+		};
 
-	return <p>Fun Date Fact: {fact}</p>;
+		fetchDateFact();
+	}, []);
+
+	const [data, setData] = useState({fact: {}, isFetching: false});
+
+	console.log(`Fact: `, data.fact);
+	return <p>Fun Date Fact: {data.fact}</p>;
+
+
 }
 
 export default App;
