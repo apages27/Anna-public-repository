0f245041bcbb911201d2c2d23611c64df66953e3
A.Pages <apages27@gmail.com>
2020-05-11 14:24:39 -0400

    Playing around with components and functions

--- react_playground/src/App.js
+++ react_playground/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Clock from './Clock-ico.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={Clock} className="App-logo" alt="logo"/>
+                    <Welcome name="Anna" />
+                </header>
+            </div>
+        );
+    }
+}
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}!</h1>;
 }
 
 export default App;
