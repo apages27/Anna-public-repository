f82ebd250eb2bba20ef9470f43448b7af5dda4f7
A.Pages <apages27@gmail.com>
2020-05-22 15:22:44 -0400

    Trying to get react API call logic working

--- react_playground/src/App.tsx
+++ react_playground/src/App.tsx
@@ -1,56 +1,96 @@
-import React, {useEffect, useState} from 'react';
-import Clock from "./Clock-ico.svg";
+import React, {Component} from "react";
 import './App.css';
 
-class App extends React.Component {
+interface IProps {
+	dateFacts?: string[];
+	onSubmit: Function;
+}
+
+interface IState {
+	dateFacts: string[];
+	month?: string;
+	day?: string;
+}
+
+const CardList = (props: any) => (
+	<div>
+		{props.dateFacts.map((dateFact: any) => <Card key={props.dateFacts.indexOf(dateFact)} {...dateFact}/>)}
+	</div>
+);
+
+class Card extends Component {
 	render() {
+		const dateFact = this.props;
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={Clock} className="App-logo" alt="logo"/>
-					<Welcome name="Anna"/>
-					<DateFact/>
-				</header>
+			<div className="github-profile">
+				<div className="info">
+					<div className="name">{dateFact}</div>
+				</div>
 			</div>
 		);
 	}
 }
 
-function Welcome(props: any) {
-	return <h1>Hello, {props.name}!</h1>;
+class Form extends Component<IProps> {
+	state = {day: "", month: ""};
+
+	handleSubmit = async (event: any) => {
+		event.preventDefault();
+		let url: string = `https://numbersapi.p.rapidapi.com/${this.state.month}/${this.state.day}/date?fragment=false&json=false`;
+
+		const response = await fetch(url, {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "numbersapi.p.rapidapi.com",
+				"x-rapidapi-key": "0173102eccmsha12a431a4720adap1c04edjsn7c69820214b9"
+			}
+		});
+		this.props.onSubmit(response.text());
+		this.setState({day: "", month: ""});
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input
+					type="text"
+					placeholder="Month"
+					value={this.state.month}
+					onChange={event => this.setState({month: event.target.value})}
+					required/>
+				<input
+					type="text"
+					placeholder="Day"
+					value={this.state.day}
+					onChange={event => this.setState({day: event.target.value})}
+					required/>
+				<button>Get Fact</button>
+			</form>
+		);
+	}
 }
 
-function DateFact() {
-	useEffect(() => {
-		const fetchDateFact = async () => {
-			let url: string = `https://numbersapi.p.rapidapi.com/12/1/date?fragment=false&json=false`;
-
-			try {
-				setData({fact: "Fetching a new date fact...", isFetching: true});
-				const response = await fetch(url, {
-					"method": "GET",
-					"headers": {
-						"x-rapidapi-host": "numbersapi.p.rapidapi.com",
-						"x-rapidapi-key": "0173102eccmsha12a431a4720adap1c04edjsn7c69820214b9"
-					}
-				});
-				console.log(`Response: `, response);
-				setData({fact: response.text, isFetching: false});
-			} catch (e) {
-				console.log(e);
-				setData({fact: e.text, isFetching: false});
-			}
+class App extends Component<IProps, IState> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			dateFacts: [],
 		};
+	}
 
-		fetchDateFact();
-	}, []);
+	addNewDateFact = (newDateFact: any) => {
+		this.setState(prevState => ({dateFacts: [...prevState.dateFacts, newDateFact]}));
+	};
 
-	const [data, setData] = useState({fact: {}, isFetching: false});
-
-	console.log(`Fact: `, data.fact);
-	return <p>Fun Date Fact: {data.fact}</p>;
-
-
+	render() {
+		return (
+			<div>
+				<div className="header">Choose a Date:</div>
+				<Form onSubmit={this.addNewDateFact}/>
+				<CardList dateFacts={this.state.dateFacts}/>
+			</div>
+		);
+	}
 }
 
 export default App;
